74247211bca76e515debdc05b4fbb7e31a514689
Scary Rat <verceldeploys@proton.me>
2024-08-19 13:33:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,4 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 
-// to configure the middle wares we use app.use()
-// read documentation for more cors objects
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-// }))
-
-
 export { app };
