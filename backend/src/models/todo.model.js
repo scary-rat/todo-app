7e8feb262016f45d95d6c2cd8ce658a956718897
Scary Rat <verceldeploys@proton.me>
2024-08-19 12:44:20 +0530

    Add files via upload

---
+++ backend/src/models/todo.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const toDoSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+})
+
+const ToDoModel = mongoose.model("todo", toDoSchema)
+
+export default ToDoModel;
