a05e255f833cf2b3d7e0b14afa7d2b9a561700a9
Scary Rat <verceldeploys@proton.me>
2024-08-19 13:26:23 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,20 +12,5 @@ const app = express();
 //     credentials: true,
 // }))
 
-app.use(cors());
-
-// configure json and set the limit of json file 
-//previously we need to use body parser npm package to get the json format now express does it directly
-app.use(express.json({ limit: "16kb" }))
-
-// urlencoded is used for decoding the %,$,_,+,%20 type of data that we get from url
-// extended for giving more nested objects we need to set extended true eg {extended: true, limits: {limit: {lim: "16kb"}}}
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-
-// to store the files and images in our own server in folder named public
-app.use(express.static("public"))
-
-// to store cookies securely in browser of client
-app.use(cookieParser())
 
 export { app };
