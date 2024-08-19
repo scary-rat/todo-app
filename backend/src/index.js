d131d4c2862fa4465a8fa0ed8eb9c13614010dc6
Scary Rat <verceldeploys@proton.me>
2024-08-19 21:54:36 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,32 +1,41 @@
+// imports
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./db/dbConnection.js";
+import { app } from "./app.js";
 import router from "./routes/toDoRoute.js";
 
+// dot env
 dotenv.config({
     path: './env'
-});
+})
 
-const app = express();
 
-// Middleware
+// middle ware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Database connection and server start
+
+app.use("/api", router)
+
+
+// database connection vye paxi matra app run garni 
 connectDB()
     .then(() => {
-        console.log("Database connected successfully");
+        const PORT = process.env.PORT || 3000;
+        app.on("error", (error) => {
+            console.log("Error", error)
+            throw error
+        })
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
     .catch((error) => {
         console.error("MONGO CONNECTION FAILED !!!", error);
         process.exit(1);
     });
 
-// Routes
-app.use("/api", router);
 
-// Export app for Vercel
-export default app;
