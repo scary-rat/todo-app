6022606041e767b879e8150ecf44dd1e66dde33e
Scary Rat <verceldeploys@proton.me>
2024-08-19 21:40:11 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,37 +5,35 @@ import cors from "cors";
 import { connectDB } from "./db/dbConnection.js";
 import { app } from "./app.js";
 import router from "./routes/toDoRoute.js";
-console.log("1");
+
 // dot env
 dotenv.config({
     path: './env'
-});
-console.log("2");
+})
+
+
 // middle ware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-console.log("3");
-// Route setup
-app.use("/api", router);
 
-// Handle database connection and server request
-let isConnected = false;
-console.log("4");
-export default async function handler(req, res) {
-    console.log("5");
-    if (!isConnected) {
-        try {
-            console.log("6");
-            await connectDB();
-            isConnected = true;
-            console.log("MongoDB connected");
-        } catch (error) {
-            console.error("MONGO CONNECTION FAILED !!!", error);
-            return res.status(500).json({ message: "Database connection failed" });
-        }
-    }
-    console.log("7");
-    // Use Express app to handle the request
-    return app(req, res);
-}
+
+// database connection vye paxi matra app run garni 
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 3000;
+        app.on("error", (error) => {
+            console.log("Error", error)
+            throw error
+        })
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MONGO CONNECTION FAILED !!!", error);
+        process.exit(1);
+    });
+
+
+app.use("/api", router)
