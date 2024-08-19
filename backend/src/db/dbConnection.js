952f8f47f2bce7cd3eabe6623c327aa2dcdf6aae
Scary Rat <verceldeploys@proton.me>
2024-08-19 13:11:04 +0530

    Update dbConnection.js

--- backend/src/db/dbConnection.js
+++ backend/src/db/dbConnection.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`);
+        const connectionInstance = await mongoose.connect(`mongodb+srv://rocks:rocks@cluster0.nvc9u.mongodb.net/todo-app?retryWrites=true&w=majority&appName=Cluster0`);
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB CONNECTION FAILED!!!", error);
