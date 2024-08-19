66adc33d9c840657808afcbc81a15d7a7b3a8224
Scary Rat <verceldeploys@proton.me>
2024-08-19 21:28:47 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,26 +5,28 @@ import cors from "cors";
 import { connectDB } from "./db/dbConnection.js";
 import { app } from "./app.js";
 import router from "./routes/toDoRoute.js";
-
+console.log("1");
 // dot env
 dotenv.config({
     path: './env'
 });
-
+console.log("2");
 // middle ware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+console.log("3");
 // Route setup
 app.use("/api", router);
 
 // Handle database connection and server request
 let isConnected = false;
-
+console.log("4");
 export default async function handler(req, res) {
+    console.log("5");
     if (!isConnected) {
         try {
+            console.log("6");
             await connectDB();
             isConnected = true;
             console.log("MongoDB connected");
@@ -33,7 +35,7 @@ export default async function handler(req, res) {
             return res.status(500).json({ message: "Database connection failed" });
         }
     }
-
+    console.log("7");
     // Use Express app to handle the request
     return app(req, res);
 }
