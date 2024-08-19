cc74363352361be6ab66529fb3fd681edf49eca3
Scary Rat <verceldeploys@proton.me>
2024-08-19 13:27:49 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(
     {
-        origin: {"https://todo-app-eta-gray.vercel.app/"},
+        origin: {"https://todo-app-eta-gray.vercel.app"},
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
